81ab2eced4992e3785fd68a3385dc579cd70ca38
pehenobra2 <peheguibra@gmail.com>
2023-08-28 12:04:49 -0300

    build

---
+++ src/app/produtos/produtos.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { IProduto, produtos } from '../produtos';
+import { ProdutosService } from '../produtos.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-produtos',
+  templateUrl: './produtos.component.html',
+  styleUrls: ['./produtos.component.css']
+})
+export class ProdutosComponent {
+
+  constructor(
+    private produtosService: ProdutosService,
+    private route: ActivatedRoute
+    ){}
+
+  produtos:IProduto[] | undefined;
+
+
+
+  ngOnInit(): void{
+    this.produtos = this.produtosService.getAll();
+    this.route.queryParamMap.subscribe(params => {
+      const descricao = params.get("descricao")?.toLowerCase();
+
+      if(descricao){
+        this.produtos = produtos.filter(produto => produto.descricao.toLowerCase().includes(descricao));
+        return;
+      }
+      this.produtos = produtos;
+    });
+  }
+
+}
